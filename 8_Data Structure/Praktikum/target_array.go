131dfaab2cda002539a849f94617e3cab3c0e26c
Merylp2 <merylputrapratama06@gmail.com>
2023-02-26 10:54:34 +0700

    tambah file

--- 8_Data Structure/Praktikum/target_array.go
+++ 8_Data Structure/Praktikum/target_array.go
@@ -30,4 +30,8 @@ func PairSum(arr []int, target int) []int {
 
 func main() {
 	fmt.Println(PairSum([]int{1, 2, 3, 4, 6}, 6))
+	fmt.Println(PairSum([]int{2, 5, 9, 11}, 11))
+	fmt.Println(PairSum([]int{1, 3, 5, 7}, 12))
+	fmt.Println(PairSum([]int{1, 4, 6, 8}, 10))
+	fmt.Println(PairSum([]int{1, 5, 6, 7}, 6))
 }
